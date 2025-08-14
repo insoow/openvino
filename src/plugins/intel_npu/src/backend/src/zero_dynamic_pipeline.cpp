3a25a60477b2d6370574bc12d6e21e69771683cb
Shumihin, Sergey <sergey.shumihin@intel.com>
2025-08-14 17:08:11 +0100

    Workaround for strides, divide it by element size to convert to element-based format

--- src/plugins/intel_npu/src/backend/src/zero_dynamic_pipeline.cpp
+++ src/plugins/intel_npu/src/backend/src/zero_dynamic_pipeline.cpp
@@ -123,11 +123,23 @@ _levelZeroOutputTensors(output_tensors)*/
             }
 
             _logger.debug(" update tensor property for input desc index: %d", desc.idx);
+
+            std::cout << desc.idx << std::endl;
+
+            // Conver byte strides to element strides
+            ov::Strides element_strides = input_tensors.at(io_index).at(0)->get_strides();
+            const size_t element_size = input_tensors.at(io_index).at(0)->get_element_type().size();
+            for (auto& stride : element_strides) {
+                stride /= element_size;  
+            }
+
+            _logger.warning("Setting argument property for input index: %zu, desc.idx: %u", io_index, desc.idx);
+
             irGraph->set_argument_property(
                 desc.idx,
                 static_cast<unsigned char*>(input_tensors.at(io_index).at(0)->data()) +
                     (i * input_tensors.at(io_index).at(0)->get_byte_size()) / _number_of_command_lists,
-                input_tensors.at(io_index).at(0)->get_strides(),
+                element_strides,
                 input_tensors.at(io_index).at(0)->get_shape());
 
             ++io_index;
@@ -135,12 +147,32 @@ _levelZeroOutputTensors(output_tensors)*/
 
         io_index = 0;
         for (const auto& desc : graph->get_output_descriptors()) {
-            _logger.debug("DynamicPipeline - update tensor property for output desc index: %d", desc.idx);
+            void* data = nullptr;
+            auto remote_tensor = std::dynamic_pointer_cast<ZeroRemoteTensor>(output_tensors.at(io_index));
+            if (remote_tensor == nullptr) {
+                data = output_tensors.at(io_index)->data();
+            } else {
+                data = remote_tensor->get_original_memory();
+            }
+
+            // graph->set_argument_value(
+            //     desc.idx,
+            //     static_cast<unsigned char*>(data) +
+            //         (i * output_tensors.at(io_index)->get_byte_size()) / _number_of_command_lists);
+
+            ov::Strides element_strides = output_tensors.at(io_index)->get_strides();
+            const size_t element_size = output_tensors.at(io_index)->get_element_type().size();
+            for (auto& stride : element_strides) {
+                stride /= element_size;  
+            }
+
+            _logger.warning("Setting argument property for output index: %zu, desc.idx: %u", io_index, desc.idx);
+
             irGraph->set_argument_property(
                 desc.idx,
                 static_cast<unsigned char*>(output_tensors.at(io_index)->data()) +
                     (i * output_tensors.at(io_index)->get_byte_size()) / _number_of_command_lists,
-                output_tensors.at(io_index)->get_strides(),
+                element_strides,
                 output_tensors.at(io_index)->get_shape());
 
             ++io_index;
