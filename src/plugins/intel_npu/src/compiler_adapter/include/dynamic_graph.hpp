9969408120a8f22528b7d809b7364f2c33c9e8cf
Insoo Woo <insoo.woo@intel.com>
2026-02-09 12:26:37 -0800

    Enable Mutable CommandList
    Signed-off-by: Insoo Woo <insoo.woo@intel.com>

--- src/plugins/intel_npu/src/compiler_adapter/include/dynamic_graph.hpp
+++ src/plugins/intel_npu/src/compiler_adapter/include/dynamic_graph.hpp
@@ -99,9 +99,12 @@ public:
                                   ze_command_queue_handle_t commandQueue,
                                   ze_fence_handle_t fence,
                                   ze_event_handle_t event,
-                                  ze_graph_profiling_pool_handle_t profiling) = 0;
+                                  ze_graph_profiling_pool_handle_t profiling,
+                                  npu_mlir_runtime_execution_context_handle_t executionContext) = 0;
         virtual void predictOutputShape(std::vector<MemRefType>& inputDescriptors,
                                         std::vector<MemRefType>& outputDescriptors) = 0;
+        virtual npu_mlir_runtime_execution_context_handle_t createExecutionContext() = 0;
+        virtual void destroyExecutionContext(npu_mlir_runtime_execution_context_handle_t) = 0;
         virtual ~Impl() {};
     };
 
@@ -151,7 +154,12 @@ public:
                  ze_command_queue_handle_t commandQueue,
                  ze_fence_handle_t inferenceFence,
                  ze_event_handle_t event,
-                 ze_graph_profiling_pool_handle_t profiling) override;
+                 ze_graph_profiling_pool_handle_t profiling,
+                 execution_context_handle_t executionContext);
+
+    void update_mutable_commandlist(const std::shared_ptr<ZeroInitStructsHolder>& zeroInitStruct,
+                 GraphArguments& args,
+                 const std::vector<uint64_t>& argIndexArray);
 
     void getBinding(GraphArguments& args) override;
 
@@ -160,6 +168,8 @@ public:
     void predict_output_shape(std::vector<MemRefType>& inputDescriptors,
                               std::vector<MemRefType>& outputDescriptors) override;
 
+    execution_context_handle_t create_execution_context();
+    void destroy_execution_context(execution_context_handle_t);
 private:
     bool release_blob(const Config& config);
     std::optional<size_t> determine_batch_size();
